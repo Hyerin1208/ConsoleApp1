4f609d4cc536a727beb8545f4b0412b7138443d2
Hyerin1208 <fghjiff8@gmail.com>
2022-05-22 21:11:28 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            var num = 1;
+            Console.Write("0~9사이의 값을 입력:");
+
+            var input = Console.ReadLine();
+
+            if (num.ToString() == input)
+            {
+                Console.WriteLine("같은값을 입력했습니다.");
+            }
+            else if (num.ToString() != input)
+            {
+                Console.WriteLine("다른값을 입력했습니다.");
+            }
+        }
+    }
+}
